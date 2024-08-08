683aa55da8cf44b03b55699ec4dfd8a02b004ade
RADshahmat <shahmatrad1998@gmai.com>
2024-08-08 10:16:16 +0600

    half landing page

---
+++ src/pages/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IntroSecHome from '../components/introSection_Home'
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+          <IntroSecHome />
+        </div>
+      );
+};
+
+export default HomePage;
