c7dd4b04b8841c8510f67a7d9ffe911c9b206392
RADshahmat <shahmatrad1998@gmai.com>
2024-10-09 23:24:31 +0600

    djdj

--- src/pages/venusPremium.js
+++ src/pages/venusPremium.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 import Header from '../components/header'; 
 import Footer from '../components/footer'; 
 import styles from '../styles/venusPremiumStyle.module.css';
 
 const VenusPremium = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 800, // Adjust as needed
+      easing: 'ease-in-out', // Smoother easing
+      once: false, 
+      mirror: true, // Allows animation to happen again when scrolling back
+      offset: 120, // Distance in pixels from the original trigger point
+    });
+  }, []);
     return (
         <div >
             <Header />
