390bac3ad6e4a2160eb566c99bfdb53aef502086
RADshahmat <shahmatrad1998@gmai.com>
2024-10-09 12:24:03 +0600

    jdj

--- src/components/homepage/HowToFindSec3.js
+++ src/components/homepage/HowToFindSec3.js
@@ -1,7 +1,7 @@
 import React from "react";
 import 'aos/dist/aos.css';
 import styles from '../../styles/homepageStyle/HowToFindSec3.module.css';
-
+import axiosInstance from '../../Axios/axios_instance';
 function HowToFindSpecialSomeone() {
 
 
